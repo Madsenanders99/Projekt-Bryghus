1addc5f8683ad0cda252c89f61af490162bd955a
Madsenanders99 <92513642+Madsenanders99@users.noreply.github.com>
2021-10-14 10:52:04 +0200

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,22 @@
+package opgave1;
+
+import java.util.Scanner;
+
+public class Exceptions {
+	public static void main(String[] args) {
+		
+		int[] prim = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
+		
+		try (Scanner scan = new Scanner(System.in)) {
+		
+			System.out.print("Hvilket nr. primtal skal vises?: ");
+			
+			int n = scan.nextInt();
+			
+			System.out.println("Primtal nr. " + n + " er " + prim[n -1] + "\n");
+			
+			} catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Indtast kun tal mellem 1 og 10");
+			}
+		}
+}
