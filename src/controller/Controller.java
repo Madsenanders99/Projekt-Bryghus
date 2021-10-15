54fd5e689f63458bd9852a7120a50ca9a7aaf9e7
Anders Madsen <eaaamads@students.eaaa.dk>
2021-10-15 09:07:05 +0200

    Controller setup

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,26 @@
 package controller;
 
+import storage.Storage;
+
 public class Controller {
+    private Storage storage;
+    private static Controller controller;
+
+    private Controller() {
+        storage = new Storage();
+    }
+
+    public static Controller getController() {
+        if (controller == null) {
+            controller = new Controller();
+        }
+        return controller;
+    }
+    public static Controller getTestController() {
+        return new Controller();
+    }
+
+    public void createObjects() {
+
+    }
 }
